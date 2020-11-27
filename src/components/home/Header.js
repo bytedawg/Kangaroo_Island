5d8f72e9d3085b2c86112ccfbf98a33cc228c8ef
Joshua S <jforceweb@yahoo.com>
2020-11-27 18:02:24 +1030

    kanga

---
+++ src/components/home/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "../globals/Hero";
+import aboutImg from "../../images/aboutBcg.jpg";
+import homeImg from "../../images/homeBcg.jpg";
+import Banner from "../globals/Banner";
+import { PrimaryBtn } from "../globals/Buttons";
+const Header = () => {
+  return (
+    <Hero img={homeImg}>
+      <Banner
+        greeting="welcome to"
+        title="Kangaroo Island Resort"
+        text="React JS Styled-Components"
+      >
+        <PrimaryBtn t="1rem">view details</PrimaryBtn>
+        {/* <PrimaryBtn as="a" href="https://www.google.com">
+          view details
+        </PrimaryBtn> */}
+      </Banner>
+    </Hero>
+  );
+};
+
+export default Header;
