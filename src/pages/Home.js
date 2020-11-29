22218cf3c4d3534d88c191a1a8d6990bc6aaec42
Joshua S <jforceweb@yahoo.com>
2020-11-29 18:30:45 +1030

    Accomodation update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/home/Header";
 import About from "../components/home/About";
-import Accomodation from "../components/home/Rooms";
+import Accomodation from "../components/home/Accomodation";
 const Home = () => {
   return (
     <>
